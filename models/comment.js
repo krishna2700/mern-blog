0c1bb95aebdca73aeb689319982dbe24f872be7c
Krishna <krishnaruparelia0207@gmail.com>
2024-11-30 21:52:40 +0530

    data modelling

---
+++ models/comment.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// schema
+const CommentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Post",
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
---
+++ models/file.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// schema
+const FileSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    public_id: {
+      type: String,
+      required: true,
+    },
+    uploaded_By: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const File = mongoose.model("File", FileSchema);
+
+module.exports = File;
